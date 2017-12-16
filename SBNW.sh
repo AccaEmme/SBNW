b34e0e2adcfacd6c4f0457f063f06596898b9976
AccaEmme <AccaEmme@users.noreply.github.com>
2017-12-16 10:56:52 +0100

    More features
    Ragazzi condivido le ultime aggiunte all'esercizio di Angelo sennò finisce che non la smetto più di aggiungere migliorie :-P
    
    Lo script oltre a gestire il controllo del MAC Address, controlla anche l'IP Privato con un messaggio di errore, un fastidioso beep generico ed esegue la funzione StaccaStaccaStacca() che se eseguito come amministratore va a disabilitare la scheda sotto osservazione scollegandoci di conseguenza dalla rete.
    Inoltre lo script va a monitorare in una voluta finestra separata anche gli utenti connessi sulla macchina e, in un'altra finestra separata, l'utilizzo di RAM e disco.
    
    Per testare lo script vi invito a eseguire su un'altro terminale:
     - Per cambiare il MAC Address -> ip link set wlp3s0 address 00:00:00:00:00:01
     - Per cambiare l'ip privato -> sudo ifconfig wlp3s0 192.168.2.123 netmask 255.255.255.0
    ( ricordo per ripristinare le cose come prima: sudo service network-manager restart )
    
    Sarebbe bello:
     1) aggiungere un menù per scegliere cosa monitorare + help
     2) permettere la modifica del MAC Address stesso nello script (previa disabilitazione e poi riabilitazione dell'interfaccia di rete)
     3) Implementare la funzione dafare() (modificandone il nome ovviamente) per vedere quali processi stanno utilizzando la scheda di rete da monitorare.
     4) Ricevere parametri esterni evitando le animazioni grafiche
     5) Consentire configurazioni generiche, settando ad esempio la variabile delaysec
     6) Al momento non va a monitorare l'IP Pubbico, ho preferito evitare di richiedere ogni tot secondi l'ip allo stesso sito potrebbe apparire come un tentativo di attacco. Sarebbe interessante però bilanciare le richieste tra N siti differenti, in modo da non richiedere esclusivamente a un unico servizio.
    
    Penso di aver giocato abbastanza su questo esercizio, se vi fa piacere lascio a voi l'implementazione degli altri esercizi, il codice è su github all'indirizzo: https://github.com/AccaEmme/SBNW/blob/master/SBNW.sh
    Happy Hacking!

--- SBNW.sh
+++ SBNW.sh
@@ -7,14 +7,81 @@
 # 10 - Chiude tutte le connessioni relative alla NIC "esposta"
 # sono ben accetti "effetti speciali" usati come alarm per avvertire l'utente che il MAC spoofing è fallito in quanto la NIC è stata ripristinata con il vero MAC Address
 
-clear
-echo "welcome..."
-sleep 3
-clear
+function status(){
+    #echo -e "\nMachine information:" ; uname -a
+    #gnome-terminal -e "bash -c \"echo -e '\nMachine information:'; uname -a; exec bash\""
 
-echo "Subliminal Message: Hack The Planet! :-P"
-sleep  1
-clear
+    #echo -e "\nUsers logged on:" ; w -h
+    #gnome-terminal -e "bash -c \"(resize -s 10 100 > /dev/null); watch -t -d -n 1 \"echo -e '\tUsers:' && w && echo -e '\n\tSpace:' && free
+
+	echo "echo $'\e[32;4m\tUsers:\e[0m' && w" > /tmp/w.tmp
+	chmod +x /tmp/w.tmp
+	gnome-terminal -e "bash -c \"(resize -s 10 100 > /dev/null); watch  -d -n 1 \"/tmp/w.tmp\"; exec bash\"" 2> /dev/null
+	#rm /tmp/status.tmp ***
+
+    #echo -e "\nCurrent date :" ; date
+
+    #echo -e "\nMachine status :" ; uptime
+
+    #echo -e "\nMemory status :" ; free
+
+    #echo -e "\nFilesystem status :"; df -h
+
+	echo "echo $'\e[32;4m\tMemory:\e[0m' && free && echo $'\e[32;4m\n\tSpace:\e[0m' && df" > /tmp/free-df.tmp
+	chmod +x /tmp/free-df.tmp
+	gnome-terminal -e "bash -c \"(resize -s 10 100 > /dev/null); watch  -d -n 1 \"/tmp/free-df.tmp\"; exec bash\"" 2> /dev/null
+}
+
+
+function warning(){
+  echo "  ___ ______________.____   __________ "
+  echo " /   |   \_   _____/|    |  \______   \\"
+  echo "/    ~    \    __)_ |    |   |     ___/"
+  echo "\    Y    /        \|    |___|    |    "
+  echo " \___|_  /_______  /|_______ \____|    "
+  echo "       \/        \/         \/         "
+
+  echo "God Save the Queen! :: ${1}"
+
+  ( speaker-test -t sine -f 1000 > /dev/null )& pid=$! ; sleep 1s ; kill -9 $pid
+}
+
+function StaccaStaccaStacca(){
+ if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo to allow disable interface:"
+    echo "sudo $0 $*"
+    exit 1
+ fi
+
+ sudo ifconfig ${devices[$device]} down
+}
+
+function WelcomeMsg(){
+ clear
+ echo "welcome..."
+ sleep 3
+ clear
+
+ for((i=0; i<20; i++));
+  do
+   LINES=$((${i}*2));
+   COLUMNS=$(((${i}+8)*2));
+   resize -s ${LINES} ${COLUMNS} > /dev/null
+   sleep 1
+  done
+
+  echo "Subliminal Message: Hack The Planet! :-P"
+  sleep  1
+  clear
+ }
+
+
+function dafare(){
+ ps -A | grep 3368 | awk '{print $4}' # gli dai il PID e ti dice il processo
+}
+
+#gnome-terminal -e "bash -c \"ls; exec bash\""
+WelcomeMsg
 
 echo "MAC Addresses here are:"
 DEVICES=$(ifconfig -a | awk 'match($0,/((.+: ))/) {print substr($0,RSTART,RLENGTH)}')
@@ -47,32 +114,44 @@ echo "Press CTRL + C to exit"
 echo -e "\n"
 sleep 3
 
+
+# start standard config
+delaysec=5
+# end standard config
+
+
 publicIP=$(curl -s https://4.ifcfg.me/)
 privateIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
-echo I\'m checking if:
+status
+
+
+echo -e "\nI\'m checking each ${delaysec} second(s) if:"
 echo -e "\t\tPublic IP: $publicIP"
 echo -e "\t\tPrivate IP: $privateIP"
 echo -e "\t\tMAC Address is still ${MACs[$device]} on ${devices[$device]}..."
 
 while(true);
- #Simuliamo il: watch -n 1 -d "ip addr show ${myarray[$device]} | grep -m 1 '${myarray[$macaddress]}'"
 do
- CURRENTMAC=$(ifconfig ${myarray[$device]} | awk 'match($0,/(..:..:..:..:..:..)/) {print substr($0,RSTART,RLENGTH)}');
- if [[ *$CURRENTMAC* != *${myarray[$macaddress]}* ]];
+ #Simuliamo il: watch -n 1 -d "ip addr show ${myarray[$device]} | grep -m 1 '${myarray[$macaddress]}'"
+ CURRENTMAC=$(ifconfig ${devices[$device]} | awk 'match($0,/(..:..:..:..:..:..)/) {print substr($0,RSTART,RLENGTH)}');
+ if [[ *$CURRENTMAC* != *${MACs[$device]}* ]];
  then
-  echo "  ___ ______________.____   __________ "
-  echo " /   |   \_   _____/|    |  \______   \\"
-  echo "/    ~    \    __)_ |    |   |     ___/"
-  echo "\    Y    /        \|    |___|    |    "
-  echo " \___|_  /_______  /|_______ \____|    "
-  echo "       \/        \/         \/         "
-
-  echo "God Save the Queen! :: MAC Address Changed!"
-
-  ( speaker-test -t sine -f 1000 > /dev/null )& pid=$! ; sleep 1s ; kill -9 $pid
+  warning "Mac Address Changed!"
+  StaccaStaccaStacca
   break;
  fi
+
+ #*** potrebbe non funzionare in presenza di due IP privati
+ CURRENTPRIVATEIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+ if [[ *$CURRENTPRIVATEIP* != *$privateIP* ]];
+ then
+  warning "Private IP Changed!"
+  StaccaStaccaStacca
+  break;
+ fi
+
+ sleep ${delaysec}
 done;
 
 
@@ -84,13 +163,3 @@ else
 fi
 
 exit 0 #  By convention, an 'exit 0' indicates success
-
-# (1)
-#Scusate per la pessima regex che ho elaborato, ma ciò che trovavo in rete non mi piaceva: 
-#((.+: )|(..:..:..:..:..:..))
-#1) ifconfig -a | grep ether | awk '{print $2}'
-#2) ip -o link show | awk -F': ' '{print "\n" $2 ": " $3 "\n"}'
-#3) solo il nome del dispositivo esclusi spazi e interfaccia lo(localhost/loopback): ifconfig -a | sed 's/[ \t].*//;/^\(lo:\|\)$/d'
-#4) ip -o link show | awk -F': ' '{print $2}'
-#5) ifconfig -a | grep -E '((.+: )|(..:..:..:..:..:..))'
-#6) ifconfig -a | awk 'match($0,/((.+: )|(..:..:..:..:..:..))/) {print substr($0,RSTART,RLENGTH)}'
